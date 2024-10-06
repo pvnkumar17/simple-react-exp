594cafc59a3c0e6aa02c819ee66cbaf270567167
Pawan K <pvnkumar17@gmail.com>
2024-10-06 21:29:13 +0530

    mindmap link to editor

--- src/components/mindmap/collapsibleTree.js
+++ src/components/mindmap/collapsibleTree.js
@@ -1,134 +1,68 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { connect } from 'react-redux';
+import { editorInfoUpdate, mindMapUpdate } from '../../actions/meAction';
 import * as d3 from "d3";
+import { fetchMemorymapData } from "../../services/meService";
+import { convertToNestedJson, convertToNestedJsonMindMap, convertToNestedJsonPrivate } from "../../utils/convertToNestedJson";
+import { cloneDeep } from 'lodash';
 
-const CollapsibleTree = ({ data }) => {
+const CollapsibleTree = ({ data, sendEditorData, setMindMapData, initialTreeData }) => {
   const svgRef = useRef(null);
 
+  const [treeData, setTreeData] = useState([]);
+
+  const findPath = (treeNode, key, path = []) => {
+    
+    if (treeNode?._id === key) {
+      return [...path, {title: treeNode.title, slug: treeNode.slug}];
+    }
+
+    if (treeNode?.children) {
+      for (let child of treeNode.children) {
+        const foundPath = findPath(child, key, [...path, {title: treeNode.title, slug: treeNode.slug}]);
+        if (foundPath) {
+          return foundPath;
+        }
+      }
+    }
+
+    return null;
+  };
+
+  const handleMindmapLinkClick = (mindmapNode) => {
+    window.location.hash = mindmapNode.slug || '';
+    const path = findPath({ children: treeData }, mindmapNode._id);
+    mindmapNode.path = path;
+    sendEditorData(mindmapNode);
+    fetchMemorymapData(mindmapNode._id).then(res => {
+      const nestedMindMapData = convertToNestedJsonMindMap(res.data);
+      setMindMapData(nestedMindMapData);
+    }
+  );
+  }
+
   useEffect(() => {
-    // const width = 960;
-    // const height = 600;
+    if (initialTreeData?.data) {
+      const copyTreeData = cloneDeep(initialTreeData.data);
+      const hierarchy = [...convertToNestedJsonPrivate(copyTreeData), ...convertToNestedJson(copyTreeData)];
+      setTreeData(hierarchy);
+     //FlatenTreeData([...copyTreeData.privateNodes, ...copyTreeData.publicNodes]);
+    }
+  }, [initialTreeData])
 
-    // const svg = d3.select(svgRef.current)
-    //   .attr("width", width)
-    //   .attr("height", height)
-    //   .attr("viewBox", [-width / 2, -height / 2, width, height])
-    //   .style("font", "10px sans-serif")
-    //   .style("user-select", "none");
-
-    // const root = d3.hierarchy(data);
-    // root.dx = 10;
-    // root.dy = width / 6;
-    // root.x0 = 0;
-    // root.y0 = 0;
-    // root.descendants().forEach((d, i) => {
-    //   d.id = i;
-    //   d._children = d.children;
-    //   if (d.depth && d.data.title.length !== 7) d.children = null;
-    // });
-
-    // const tree = d3.tree().nodeSize([root.dx, root.dy]);
-
-    // const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
-
-    // const g = svg.append("g")
-    //   .attr("font-family", "sans-serif")
-    //   .attr("font-size", 10)
-    //   .attr("transform", `translate(${root.dy / 3},${root.dx - 100})`);
-
-    // const update = (source) => {
-    //   const nodes = root.descendants().reverse();
-    //   const links = root.links();
-
-    //   tree(root);
-
-    //   let left = root;
-    //   let right = root;
-    //   root.eachBefore(node => {
-    //     if (node.x < left.x) left = node;
-    //     if (node.x > right.x) right = node;
-    //   });
-
-    //   const height = right.x - left.x + root.dx * 2;
-
-    //   const transition = svg.transition()
-    //     .duration(250)
-    //     .attr("viewBox", [-root.dy / 3, left.x - root.dx, width, height])
-    //     .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
-
-    //   const node = g.selectAll("g")
-    //     .data(nodes, d => d.id);
-
-    //   const nodeEnter = node.enter().append("g")
-    //     .attr("transform", d => `translate(${source.y0},${source.x0})`)
-    //     .attr("fill-opacity", 0)
-    //     .attr("stroke-opacity", 0)
-    //     .on("click", (event, d) => {
-    //       d.children = d.children ? null : d._children;
-    //       update(d);
-    //     });
-
-    //   nodeEnter.append("circle")
-    //     .attr("r", 2.5)
-    //     .attr("fill", d => d._children ? "#555" : "#999")
-    //     .attr("stroke-width", 10);
-
-    //   nodeEnter.append("text")
-    //     .attr("dy", "0.31em")
-    //     .attr("x", d => d._children ? -6 : 6)
-    //     .attr("text-anchor", d => d._children ? "end" : "start")
-    //     .text(d => d.data.title)
-    //     .clone(true).lower()
-    //     .attr("stroke-linejoin", "round")
-    //     .attr("stroke-width", 3)
-    //     .attr("stroke", "white");
-
-    //   node.merge(nodeEnter).transition(transition)
-    //     .attr("transform", d => `translate(${d.y},${d.x})`)
-    //     .attr("fill-opacity", 1)
-    //     .attr("stroke-opacity", 1);
-
-    //   node.exit().transition(transition).remove()
-    //     .attr("transform", d => `translate(${source.y},${source.x})`)
-    //     .attr("fill-opacity", 0)
-    //     .attr("stroke-opacity", 0);
-
-    //   const link = g.selectAll("path")
-    //     .data(links, d => d.target.id);
-
-    //   const linkEnter = link.enter().append("path")
-    //     .attr("d", d => {
-    //       const o = { x: source.x0, y: source.y0 };
-    //       return diagonal({ source: o, target: o });
-    //     });
-
-    //   link.merge(linkEnter).transition(transition)
-    //     .attr("d", diagonal);
-
-    //   link.exit().transition(transition).remove()
-    //     .attr("d", d => {
-    //       const o = { x: source.x, y: source.y };
-    //       return diagonal({ source: o, target: o });
-    //     });
-
-    //   root.eachBefore(d => {
-    //     d.x0 = d.x;
-    //     d.y0 = d.y;
-    //   });
-    // };
-
-    // update(root);
+  useEffect(() => {
     // Specify the charts’ dimensions. The height is variable, depending on the layout.
   const width = 928;
-  const marginTop = 10;
-  const marginRight = 10;
-  const marginBottom = 10;
-  const marginLeft = 40;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 20;
+  const marginLeft = 50;
 
   // Rows are separated by dx pixels, columns by dy pixels. These names can be counter-intuitive
   // (dx is a height, and dy a width). This because the tree must be viewed with the root at the
   // “bottom”, in the data domain. The width of a column is based on the tree’s height.
   const root = d3.hierarchy(data);
-  const dx = 10;
+  const dx = 40;
   const dy = (width - marginRight - marginLeft) / (1 + root.height);
 
   // Define the tree layout and the shape for links.
@@ -141,7 +75,7 @@ const CollapsibleTree = ({ data }) => {
       svg.attr("width", width)
       .attr("height", dx)
       .attr("viewBox", [-marginLeft, -marginTop, width, dx])
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; user-select: none;");
+      .attr("style", "max-width: 100%; height: auto; font: 14px sans-serif; user-select: none;");
 
   const gLink = svg.append("g")
       .attr("fill", "none")
@@ -152,6 +86,17 @@ const CollapsibleTree = ({ data }) => {
   const gNode = svg.append("g")
       .attr("cursor", "pointer")
       .attr("pointer-events", "all");
+
+  const getT = (d) => {
+    if(d.data.type === "file"){
+      const textDetails = d.data.dataDetails.text &&  JSON.parse(d.data.dataDetails.text)?.root?.children?.filter(item => item.type === 'heading') || [];
+      const titleText = textDetails.length && textDetails.map(item => `<div><a>${item.children[0].text}</a></div>`);
+      return `<div><div>${d.data.title}</div> <span>${titleText?.toString()}</span></div>`;
+      //return `<div><div>${d.data.title}</div> ${titleText?.toString()}</div>`;
+    }else {
+      return `<div>${d.data.title}</div>`
+    }
+  }
 
   function update(event, source) {
     const duration = event?.altKey ? 2500 : 250; // hold the alt key to slow down the transition
@@ -191,19 +136,36 @@ const CollapsibleTree = ({ data }) => {
         });
 
     nodeEnter.append("circle")
-        .attr("r", 2.5)
+        .attr("r", 5.5)
         .attr("fill", d => d._children ? "#555" : "#999")
         .attr("stroke-width", 10);
 
-    nodeEnter.append("text")
-        .attr("dy", "0.31em")
-        .attr("x", d => d._children ? -6 : 6)
-        .attr("text-anchor", d => d._children ? "end" : "start")
-        .text(d => d.data.title)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-width", 3)
-        .attr("stroke", "white")
-        .attr("paint-order", "stroke");
+    // nodeEnter.append("text")
+    // //nodeEnter.append("foreignObject")
+    //     .attr("dy", "0.31em")
+    //     .attr("x", d => d._children ? -6 : 6)
+    //     .attr("text-anchor", d => d._children ? "end" : "start")
+    //     //.text(d => d.data.title)
+    //     .text(d => getT(d))
+    //     //.html(d => getT(d))
+    //     .attr("stroke-linejoin", "round")
+    //     .attr("stroke-width", 3)
+    //     .attr("stroke", "white")
+    //     .attr("paint-order", "stroke");
+
+    nodeEnter.append("foreignObject")
+    .attr("width", 120) // Adjust width as needed
+    .attr("height", 40) // Adjust height as needed
+    .append("xhtml:div")
+    .style("font", "14px sans-serif")
+    .style("color", d => (d._children ? "#555" : "#999"))
+    .html(d => getT(d))
+    .on("click", function(d) {
+      // Handle the click event
+      if(d.currentTarget.__data__.data.type === "file"){
+        handleMindmapLinkClick(d.currentTarget.__data__.data);
+      }
+  });;
 
     // Transition nodes to their new position.
     const nodeUpdate = node.merge(nodeEnter).transition(transition)
@@ -260,5 +222,16 @@ const CollapsibleTree = ({ data }) => {
 
   return <svg ref={svgRef}></svg>;
 };
+function mapStateToProps(state, ownProps) {
+  return {
+    initialTreeData: state.meDetails.user
+  };
+}
 
-export default CollapsibleTree;
+function mapDispatchToProps(dispatch) {
+  return {
+    sendEditorData: (data) => dispatch(editorInfoUpdate(data)),
+    setMindMapData: (data) => dispatch(mindMapUpdate(data))
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CollapsibleTree);
